de7452d4e37d3885b96c38f058f3b637e551893b
zoickx <zoickx@ztld.org>
2019-09-15 17:28:09 +0300

    Update org config, add term config, simplify some others

--- emacsd/packages.el
+++ emacsd/packages.el
@@ -24,6 +24,14 @@
 ;; ranger
   (global-set-key (kbd "C-x C-r") 'ranger)
 
+;; evil
+  ;order is important here
+  (setq evil-want-abbrev-expand-on-insert-exit nil)
+  (setq evil-want-C-u-scroll t)
+  (require 'evil)
+  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (evil-mode 1)
+
 ;; magit
   (global-set-key (kbd "C-x C-m") 'magit-status)
 
@@ -31,33 +39,38 @@
   (which-key-mode)
 
 ;; org-mode
-  ;; pretty bullets everywhere
-  (require 'org-bullets)
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode t)))
-  ;; set default org directory
-  (setq org-dir "~/org")
-  ;; set a default file and a capture command
-  (setq org-default-notes-file (concat org-dir "/inbox.org"))
-  (define-key global-map "\C-cc" 'org-capture)
+  ; sane indentation for lists
+  (setq org-startup-indented t)
+  ; symbol to be shown over headings with content
+  (setq org-ellipsis "ˇ")
+  ; add timestamps for task completion
+  (setq org-log-done 'time)
+  ; allow whitespace between items
+  (setq org-cycle-separator-lines -1)
+  ; directory org-todos are gathered from)
+  (setq org-agenda-files '("~/org/project"))
 
-;; evil
-  ;; order is important here
-  (setq evil-want-abbrev-expand-on-insert-exit nil)
-  (setq evil-want-C-u-scroll t)
-  (require 'evil)
-  (define-key evil-normal-state-map (kbd "M-.") nil)
-  (evil-mode 1)
+  (with-eval-after-load 'org
+    (define-key org-mode-map (kbd "C-c C-<return>") 'org-todo))
 
-;; proof general + coq + company-coq
+;; proof general + company-coq
   (setq proof-splash-enable nil)
   (add-hook 'coq-mode-hook 'company-coq-mode)
   (add-hook 'coq-mode-hook 'undo-tree-mode)
+  (put 'company-coq-fold 'disabled nil)
 
 ;; rust + racer
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
-  (require 'rust-mode)
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (add-hook 'rust-mode-hook 'racer-mode)
+  (add-hook 'racer-mode-hook 'eldoc-mode)
+  (add-hook 'racer-mode-hook 'company-mode)
   (setq company-tooltip-align-annotations t)
-  (define-key rust-mode-map (kbd "C-c C-d") #'racer-describe)
+  (with-eval-after-load 'rust-mode
+    (progn
+      (define-key rust-mode-map (kbd "TAB") 'company-indent-or-complete-common)
+      (define-key rust-mode-map (kbd "C-c C-d") 'racer-describe)))
+
+;; term
+  ; use C-x as escape char
+  (defun term-c-x-escape (&rest ignored)
+    (term-set-escape-char 24))
+  (advice-add 'term :after #'term-c-x-escape)
