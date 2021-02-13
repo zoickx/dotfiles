d429fe6724ea83340a01389135e851dd1084d64f
Ilia Zaichuk <zoickx@ztld.org>
2021-02-13 23:51:35 +0200

    Ignore /var/lib/pacman

--- aconfmgr/00-ignore.sh
+++ aconfmgr/00-ignore.sh
@@ -1,3 +1,4 @@
 IgnorePath '/boot/*' # completely auto-generated junk, should neither change, nor influence much
 IgnorePath '/etc/ca-certificates/*'
 IgnorePath '/etc/ssl/*'
+IgnorePath '/var/lib/pacman/*' # I don't actually know what this is, but it doesn't look meaningful C:
