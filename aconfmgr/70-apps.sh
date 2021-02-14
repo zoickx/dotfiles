d2aface1f0ae95979ff60a880a9a95274d5c31d3
Ilia Zaichuk <zoickx@ztld.org>
2021-02-14 21:19:47 +0200

    Add some CLI apps

--- aconfmgr/70-apps.sh
+++ aconfmgr/70-apps.sh
@@ -1,2 +1,19 @@
-## Actual GUI apps
+## CLI utilities
+
+# u want mah files? well come and get 'em!
+AddPackage cryfs # Cryptographic filesystem for the cloud
+AddPackage gocryptfs # Encrypted overlay filesystem written in Go.
+AddPackage veracrypt # Disk encryption with strong security based on TrueCrypt
+AddPackage --foreign srm # A secure replacement for rm(1) that overwrites data before unlinking
+
+# stuffs
+AddPackage xclip # Command line interface to the X11 clipboard
+AddPackage scrot # Simple command-line screenshot utility for X
+AddPackage youtube-dl # A command-line program to download videos from YouTube.com and a few more sites
+AddPackage rmlint # Tool to remove duplicates and other lint, being much faster than fdupes
+AddPackage --foreign tealdeer-bin # A fast tldr client in Rust. Static binaries from GitHub.
+
+
+## Actual proper GUI apps
+
 AddPackage firefox # Standalone web browser from mozilla.org
