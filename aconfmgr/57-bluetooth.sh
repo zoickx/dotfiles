46e6a6c7235456fed01e0adf0ec55066f5c9fe5b
Ilia Zaichuk <zoickx@ztld.org>
2021-02-14 20:38:00 +0200

    Enable bluetooth + bluetooth sound

---
+++ aconfmgr/57-bluetooth.sh
@@ -0,0 +1,15 @@
+## Bluetooth stuffs
+
+# note: bluetooth is actually managed by the [bluez] package,
+# automatically installed as a dependency of [blueman], the manager
+AddPackage blueman # GTK+ Bluetooth Manager
+AddPackage pulseaudio-bluetooth # Bluetooth support for PulseAudio
+
+# [systemctl enable bluetooth.service]
+CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
+CreateLink /etc/systemd/system/dbus-org.bluez.service /usr/lib/systemd/system/bluetooth.service
+
+# I believe these are responsible for keeping track of actual connections
+# I'm fine with doing that imperatively
+IgnorePath '/var/lib/blueman/*'
+IgnorePath '/var/lib/bluetooth/*'
