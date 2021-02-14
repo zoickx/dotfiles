43064bb0c929000d02160428d84525a658be4cc2
Ilia Zaichuk <zoickx@ztld.org>
2021-02-14 20:19:11 +0200

    Disable PC speaker

--- aconfmgr/55-sound.sh
+++ aconfmgr/55-sound.sh
@@ -4,3 +4,6 @@ AddPackage pavucontrol # PulseAudio Volume Control
 
 # this was created by Arch; according to ArchWiki, pulseaudio is enabled by default
 CreateLink /etc/systemd/user/sockets.target.wants/pulseaudio.socket /usr/lib/systemd/user/pulseaudio.socket
+
+# disable PC speaker (https://wiki.archlinux.org/index.php/PC_speaker#Globally)
+CopyFile /etc/modprobe.d/nobeep.conf
