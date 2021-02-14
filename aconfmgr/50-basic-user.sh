2021aea9e8c1cb6c4ea92e7247c88a323fd5e96f
Ilia Zaichuk <zoickx@ztld.org>
2021-02-14 17:07:57 +0200

    Add basic userspace utilities; switch to zsh

--- aconfmgr/50-basic-user.sh
+++ aconfmgr/50-basic-user.sh
@@ -1,2 +1,21 @@
-AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+
+## Basics not included in base-devel for some reason
+
+AddPackage wget # Network utility to retrieve files from the Web
+AddPackage zip # Compressor/archiver for creating and modifying zipfiles
+AddPackage unzip # For extracting and viewing files in .zip archives
+AddPackage htop # Interactive process viewer
+
+## The everpresent Rust fleet
+
+AddPackage bat # Cat clone with syntax highlighting and git integration
+AddPackage dua-cli # A tool to conveniently learn about the disk usage of directories, fast!
+AddPackage exa # ls replacement
+AddPackage fd # Simple, fast and user-friendly alternative to find
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+
+## QOL
+
+AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
