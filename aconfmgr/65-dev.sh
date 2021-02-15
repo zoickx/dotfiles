15b1c4c3db5fe7cbcde9c8ce258eca66bb202f7c
Ilia Zaichuk <zoickx@ztld.org>
2021-02-15 16:51:49 +0200

    Add opam (+ some dependencies of Helix)

---
+++ aconfmgr/65-dev.sh
@@ -0,0 +1,5 @@
+AddPackage opam # OCaml package manager
+
+# Weird dependencies of (dependencies of) [Helix](https://github.com/vzaliva/helix)
+AddPackage perl-ipc-system-simple # Run commands simply, with detailed diagnostics
+AddPackage perl-string-shellquote # Quote strings for passing through the shell
