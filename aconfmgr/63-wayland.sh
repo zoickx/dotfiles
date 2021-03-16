210c3d14ea6638699f7a2ca3888e1f811711d5d9
Ilia Zaichuk <zoickx@ztld.org>
2021-03-16 18:17:05 +0200

    State of wayland 2021

---
+++ aconfmgr/63-wayland.sh
@@ -0,0 +1,16 @@
+AddPackage sway # Tiling Wayland compositor and replacement for the i3 window manager
+
+AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
+
+AddPackage light # Program to easily change brightness on backlight-controllers.
+AddPackage gammastep # Adjust the color temperature of your screen according to your surroundings.
+AddPackage grim # Screenshot utility for Wayland
+AddPackage slurp # Select a region in a Wayland compositor
+AddPackage wf-recorder # Screen recorder for wlroots-based compositors such as sway
+
+AddPackage --foreign sway-launcher-desktop # TUI Application launcher with Desktop Entry support.
+
+# meh
+AddPackage xorg-xwayland # run X clients under wayland
