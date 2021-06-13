37842665cb50d7e97e15719d2858461bc1f0ee84
Ilia Zaichuk <zoickx@ztld.org>
2021-06-13 20:26:23 +0300

    Remove workaround fixed by using pgtk emacs

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -8,6 +8,3 @@
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
-;;; at least don't blink full white on startup
-(set-face-background 'default "black")
-(set-face-background 'mode-line "black")
