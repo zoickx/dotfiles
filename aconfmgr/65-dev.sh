5d07309bbc2e7a247bcbde4e7b5a614cc1f8807b
Ilia Zaichuk <zoickx@ztld.org>
2021-09-14 17:45:30 +0300

    Add TeX packages

--- aconfmgr/65-dev.sh
+++ aconfmgr/65-dev.sh
@@ -7,3 +7,19 @@ AddPackage perl-ipc-system-simple # Run commands simply, with detailed diagnosti
 AddPackage perl-string-shellquote # Quote strings for passing through the shell
 
 AddPackage openssh # Premier connectivity tool for remote login with the SSH protocol
+
+# LaTeX: [texlive-most] group. (groups not supported by aconfmgr it seems)
+AddPackage texlive-bibtexextra # TeX Live - Additional BibTeX styles and bibliography databases
+AddPackage texlive-core # TeX Live core distribution
+AddPackage texlive-fontsextra # TeX Live - all sorts of extra fonts
+AddPackage texlive-formatsextra # TeX Live - collection of extra TeX 'formats'
+AddPackage texlive-games # TeX Live - Setups for typesetting various board games, including chess
+AddPackage texlive-humanities # TeX Live - LaTeX packages for law, linguistics, social sciences, and humanities
+AddPackage texlive-latexextra # TeX Live - Large collection of add-on packages for LaTeX
+AddPackage texlive-music # TeX Live - Music typesetting packages
+AddPackage texlive-pictures # TeX Live - Packages for drawings graphics
+AddPackage texlive-pstricks # TeX Live - Additional PSTricks packages
+AddPackage texlive-publishers # TeX Live - LaTeX classes and packages for specific publishers
+AddPackage texlive-science # TeX Live - Typesetting for mathematics, natural and computer sciences
+IgnorePath '/etc/texmf/*'
+IgnorePath '/usr/share/texmf-dist/*'
