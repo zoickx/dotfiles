ae393b6893d69c27ac31cbbfb7db4a99e77cfc97
Ilia Zaichuk <zoickx@ztld.org>
2022-02-17 14:21:07 +0200

    Add wireguard
    (+ traceroute - doesn't hurt)

--- aconfmgr/25-network.sh
+++ aconfmgr/25-network.sh
@@ -5,3 +5,6 @@ CreateLink /etc/systemd/system/multi-user.target.wants/dhcpcd.service /usr/lib/s
 CreateLink /etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlp5s0.service /usr/lib/systemd/system/wpa_supplicant@.service
 ## configuration file with sensitive network data, not committed to git; manage manually
 IgnorePath '/etc/wpa_supplicant/wpa_supplicant-wlp5s0.conf'
+
+AddPackage traceroute # Tracks the route taken by packets over an IP network
+AddPackage wireguard-tools # next generation secure network tunnel - tools for configuration
