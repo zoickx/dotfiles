cbe4ce81f0aa46a2a36f35c0304b67a530a19136
Ilia Zaichuk <zoickx@ztld.org>
2022-03-26 19:56:00 +0200

    Clean up pipewire in aconfmgr

--- aconfmgr/55-sound.sh
+++ aconfmgr/55-sound.sh
@@ -1,14 +1,14 @@
+# disable PC speaker (https://wiki.archlinux.org/index.php/PC_speaker#Globally)
+CopyFile /etc/modprobe.d/nobeep.conf
+
+# PipeWire for all sound
 AddPackage pipewire # Low-latency audio/video router and processor
 AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
 AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
-
-AddPackage pavucontrol # PulseAudio Volume Control
-
+IgnorePath /usr/share/pipewire/pipewire.conf
 CreateLink /etc/systemd/user/pipewire-session-manager.service /usr/lib/systemd/user/pipewire-media-session.service
 CreateLink /etc/systemd/user/pipewire.service.wants/pipewire-media-session.service /usr/lib/systemd/user/pipewire-media-session.service
 CreateLink /etc/systemd/user/sockets.target.wants/pipewire-pulse.socket /usr/lib/systemd/user/pipewire-pulse.socket
 
-CopyFile /usr/share/pipewire/pipewire.conf
-
-# disable PC speaker (https://wiki.archlinux.org/index.php/PC_speaker#Globally)
-CopyFile /etc/modprobe.d/nobeep.conf
+# Graphical audio control panel (pipewire is compatible with this)
+AddPackage pavucontrol # PulseAudio Volume Control
