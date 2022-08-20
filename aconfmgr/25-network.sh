a10198b40e97dfdf5376ef330f240bd19e985282
Ilia Zaichuk <zoickx@ztld.org>
2022-08-20 19:32:37 +0300

    Enable NetworkManager.service

--- aconfmgr/25-network.sh
+++ aconfmgr/25-network.sh
@@ -1,11 +1,11 @@
-AddPackage networkmanager # Network connection manager and user applications
 # Note: WiFi is managed by [wpa_supplicant], installed as a dependency of networkmanager
 #       DHCP is managed by networkmanager's internal client (no need for [dhcpcd] or [dhclient])
 
-## [systemctl enable NetworkManager.service] ?
-# CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service /usr/lib/systemd/system/NetworkManager.service.
-# CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service /usr/lib/systemd/system/NetworkManager-dispatcher.service.
-# CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service /usr/lib/systemd/system/NetworkManager-wait-online.service.
+AddPackage networkmanager # Network connection manager and user applications
+# [systemctl enable NetworkManager.service]
+CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service /usr/lib/systemd/system/NetworkManager.service.
+CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service /usr/lib/systemd/system/NetworkManager-dispatcher.service.
+CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service /usr/lib/systemd/system/NetworkManager-wait-online.service.
 
 AddPackage traceroute # Tracks the route taken by packets over an IP network
 AddPackage wireguard-tools # next generation secure network tunnel - tools for configuration
